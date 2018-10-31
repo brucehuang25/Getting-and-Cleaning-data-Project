eeacc3e3312d8c6705dd98e4216017aab32c3273
ignacio-alorre <ignacio.alorre@gmail.com>
2018-10-31 18:23:10 +0400

    Update run-analysis.R

--- run-analysis.R
+++ run-analysis.R
@@ -1,3 +1,16 @@
+library(reshape2)
+
+filename <- "getdata_dataset.zip"
+
+## Download and unzip the dataset:
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
 #Considering we have in local our test and train dataset
 #The first thing is to store those tables into variables
 
